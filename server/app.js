8c6e5b80c4ae5d39ba1d27aa197e1227c40aebf9
GustafJ <gustaf89@hotmail.se>
2021-10-05 10:06:32 +0200

    add routes and controllers

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const productRoutes = require('./routes/products');
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
+
+app.use('/products', productRoutes);
 
 app.listen(3000);
