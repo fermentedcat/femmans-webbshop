72540ffd37344d6f67957452e705c8d8483a655d
GustafJ <gustaf89@hotmail.se>
2021-10-05 22:11:56 +0200

    add getAllUsers

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,1 +1,9 @@
-//TBA
+const express = require('express');
+
+const { getAllUsers } = require('../controllers/users')
+
+const router = express.Router();
+
+router.get('/', getAllUsers);
+
+module.exports = router;
