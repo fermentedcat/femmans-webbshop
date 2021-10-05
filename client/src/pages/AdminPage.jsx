0db7bb18af265ad3dff5eacab8241b52e39c24cb
Axel Thiel <axth2220@gmail.com>
2021-10-05 21:32:53 +0200

    add landingpage and admin page

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Grid from '@mui/material/Grid';
 import { Sidebar } from '../components/Sidebar/Sidebar';
+import { AdminMain } from '../components/AdminMain/AdminMain';
 
 export const AdminPage = () => {
-    return (
-        <Grid container>
-            <Grid item xs={3}><Sidebar /></Grid>
-            <Grid item xs={9}>Stuff</Grid>
-        </Grid>
-    )
+  const [data, setData] = useState([]);
+
+  const fetchData = (endpoint) => {
+    fetch(`http://localhost:3000/api/${endpoint}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setData(data)
+      })
+  }
+
+  return (
+    <Grid container>
+      <Grid item xs={3}>
+        <Sidebar fetchData={fetchData} />
+      </Grid>
+      <Grid item xs={9}>
+        <AdminMain data={data} />
+      </Grid>
+    </Grid>
+  )
 }
