1a81a17f187648351f1ab7ad63b2831844beab2f
GustafJ <gustaf89@hotmail.se>
2021-10-05 21:22:15 +0200

    add 404 if no users

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -5,7 +5,9 @@ exports.getAllProducts = (req, res, next) => {
     .populate('categories')
     .exec((err, products) => {
       if (err) res.status(400).end();
-      res.status(200).json(products).end();
+
+      if (users) res.status(200).json(products).end();
+      else res.status(404).end();
     });
 }
 
