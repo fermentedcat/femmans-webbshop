08b2e449be11ee94ac4eb6534379e67e87ddd365
Maja Thunberg <majaneko@gmail.com>
2021-10-05 11:23:40 +0200

    Add empty files

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
-const productRoutes = require('./routes/products');
+const productsRoutes = require('./routes/products');
+// const usersRoutes = require('./routes/users');
+// const ordersRoutes = require('./routes/orders');
+// const categoriesRoutes = require('./routes/categories');
 
 const app = express();
 
@@ -8,6 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-app.use('/products', productRoutes);
+app.use('/api/products', productsRoutes);
+// app.use('/api/users', usersRoutes);
+// app.use('/api/orders', ordersRoutes);
+// app.use('/api/categories', categoriesRoutes);
+app.use('/', (req, res, next) => {
+  res.status(404).send('Not found');
+})
 
 app.listen(3000);
