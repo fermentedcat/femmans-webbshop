4162474c5ea9fa2ab653116fa34abf72b7f0b6c4
Axel Thiel <axth2220@gmail.com>
2021-10-05 17:36:41 +0200

    setup router and add admin page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import {Switch, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
+    <>
     <div>Hej</div>
+    <Switch >
+      <Route path="/admin" component={}>
+        <TestComponent />
+      </Route>
+    </Switch>
+    </>
   );
 } 
 
