d188b8717da99856b7c88d78a35c56e5427af2ea
Robin Hedlund <robin.live@live.se>
2021-10-06 17:07:54 +0200

    "add getOneProduct route and controller"

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-const { getAllProducts, addNewProduct } = require('../controllers/products');
+const { getAllProducts, addNewProduct, getOneProduct } = require('../controllers/products');
 
 const router = express.Router();
 
 router.get('/', getAllProducts);
+router.get('/:id', getOneProduct);
 router.post('/', addNewProduct);
 
 module.exports = router;
