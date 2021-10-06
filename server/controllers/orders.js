ee900401d7414541bf53cce972d56b62bf2d7c23
Dragan Sälg <dragan.salg@gmail.com>
2021-10-06 10:46:38 +0200

    add newOrder function and fix skånsk kod

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -9,3 +9,28 @@ exports.getAllOrders = (req, res, next) => {
       res.json(orders);
     })
 }
+
+exports.addNewOrder = (req, res, next) => {
+  const orderData = {
+    user: '615d579c23f83bc71e9fc42e', 
+    orderRows: {
+      product: '615d5d2ba6ef1c864de35b81',
+      amount: 1, 
+      priceEach: 1000000
+    },
+    address: {
+      street: 'Stoftgatan',
+      postalCode: '08080',
+      city: 'Stockholm',
+      country: 'Sverige'
+    },
+  }
+  const order = new Order(orderData);
+
+  order.save()
+    .then(() => {
+      res.status(201);
+    }).catch(err => {
+      res.status(400).end()
+    });
+}
--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
-const { getAllOrders } = require('../controllers/orders');
+const { getAllOrders, addNewOrder } = require('../controllers/orders');
 
 const router = express.Router();
 
 router.get('/', getAllOrders);
+router.post('/', addNewOrder);
 
 module.exports = router;
