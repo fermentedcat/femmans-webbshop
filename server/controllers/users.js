30316d736e853b30e4a76c201fc35d6af1aa33e0
GustafJ <gustaf89@hotmail.se>
2021-10-07 10:06:30 +0200

    add get-one-user

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,9 +3,9 @@ const User = require('../models/User');
 exports.getAllUsers = (req, res, next) => {
   User.find()
     .exec((err, users) => {
-      if(err) res.sendStatus(400);
+      if (err) res.sendStatus(400);
 
-      if(users) res.status(200).json(users);
+      if (users) res.status(200).json(users);
       else res.sendStatus(404);
     });
 }
@@ -17,6 +17,20 @@ exports.addNewUser = (req, res, next) => {
   newUser.save()
     .then(() => {
       res.sendStatus(201);
+    })
+    .catch(err => {
+      res.status(400).end();
+    });
+}
+
+exports.getOneUser = (req, res, next) => {
+  const id = req.params.id;
+
+  User.findById(id)
+    .select('-password')
+    .then((user) => {
+      if (user) res.status(200).json(user);
+      else res.status(404).end();
     })
     .catch(err => {
       res.status(400).end();
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-const { getAllUsers, addNewUser } = require('../controllers/users')
+const { getAllUsers, addNewUser, getOneUser } = require('../controllers/users')
 
 const router = express.Router();
 
 router.get('/', getAllUsers);
+router.get('/:id', getOneUser);
 router.post('/', addNewUser);
 
 module.exports = router;
