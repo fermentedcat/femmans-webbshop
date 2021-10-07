00744d0a4cdbeaa3d4d1ed1627f21d5722caea8b
Dragan Sälg <dragan.salg@gmail.com>
2021-10-07 10:37:28 +0200

    add deleteOneUser function'

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
-const { getAllUsers, addNewUser, getOneUser } = require('../controllers/users')
+const { getAllUsers, addNewUser, getOneUser, deleteOneUser } = require('../controllers/users')
 
 const router = express.Router();
 
 router.get('/', getAllUsers);
 router.get('/:id', getOneUser);
 router.post('/', addNewUser);
+router.delete('/:id', deleteOneUser);
 
 module.exports = router;
