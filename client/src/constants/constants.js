bf02362d09ab5a03465d8eb73838a0e21c796dbd
Axel Thiel <axth2220@gmail.com>
2021-10-08 16:23:34 +0200

    add data category constant

---
+++ client/src/constants/constants.js
@@ -0,0 +1,6 @@
+export const DATA_CATEGORIES = [
+  { category: 'Products', endpoint: 'products' },
+  { category: 'Orders', endpoint: 'orders' },
+  { category: 'Customers', endpoint: 'customers' },
+  { category: 'Categories', endpoint: 'categories' }
+]
