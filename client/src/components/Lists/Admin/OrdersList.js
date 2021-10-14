4d6f3a19d3491fbdf30af033c929896c5182cf9b
Axel Thiel <axth2220@gmail.com>
2021-10-14 10:03:13 +0200

    change fetch function back to original

--- client/src/components/Lists/Admin/OrdersList.js
+++ client/src/components/Lists/Admin/OrdersList.js
@@ -6,17 +6,16 @@ import List from '@mui/material/List';
 import { OrderListItem } from './OrderListItem';
 
 export const OrdersList = () => {
-  const {data: orders, /* error, */ updateData} = useFetch(getOrders);
-
+  const {data: orders, error} = useFetch(getOrders, 'token');
 
   return (
     <List dense>
       {orders &&
-        orders.map((order, index) => {
+        orders.map((order) => {
           return (
             <OrderListItem
               key={index}
-              order={order}
+              order={order._id}
               afterUpdate={updateData}
             />
           );
