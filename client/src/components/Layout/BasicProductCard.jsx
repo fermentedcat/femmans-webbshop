8b95d1026ccff1582c326758158ddc50267c9dba
GustafJ <gustaf89@hotmail.se>
2021-10-20 10:38:59 +0200

    work on show all products

--- client/src/components/Layout/BasicProductCard.jsx
+++ client/src/components/Layout/BasicProductCard.jsx
@@ -1,25 +1,62 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
-const BasicCard = ({ product }) => {
+export const BasicProductCard = ({ product }) => {
+  const history = useHistory();
+
+  const buyProduct = (e) => {
+    e.stopPropagation();
+    //redirect if user not logged in
+    //fetch add to cart 
+  };
+
+  const redirectToDetails = (e) => {
+    history.push(`/product/${product._id}`);
+  };
+
   return (
-    <Card sx={{ width: 300, height: 300 }}>
+    <Card
+      sx={{ width: 250, height: 375, cursor: 'pointer' }}
+      onClick={redirectToDetails}
+    >
       <CardMedia
         component='img'
         alt={product.title}
-        height='140'
+        height='250'
         image={product.photos[0]}
       />
       <CardContent>
-        <Typography gutterBottom variant='h5' component='div'>
+        <Typography
+          sx={{
+            width: '100%',
+            whiteSpace: 'nowrap',
+            textOverflow: 'ellipsis',
+            overflow: 'hidden',
+          }}
+          gutterBottom
+          variant='h5'
+          component='div'
+        >
           {product.title}
         </Typography>
       </CardContent>
+      <Button
+        sx={{ width: '90%', margin: '0 5%' }}
+        id='button'
+        variant='contained'
+        onClick={buyProduct}
+      >
+        <AddShoppingCartIcon sx={{ mr: '3px' }} />
+        <Typography>{product.price} kr</Typography>
+      </Button>
     </Card>
   );
 };
 
-export default BasicCard;
+export default BasicProductCard;
--- client/src/pages/ProductsPage.jsx
+++ client/src/pages/ProductsPage.jsx
@@ -4,19 +4,23 @@ import { getProducts } from '../api/api.js';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import BasicProductCard from '../components/Layout/BasicProductCard';
-import { Typography } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 export const ProductsPage = () => {
   const { data, setData, errors } = useFetch(getProducts);
+
   return (
     <Box>
       <Typography variant='h3'>Alla Produkter</Typography>
-      <Grid container>
+      <Grid container spacing={4} sx={{ justifyContent: 'center' }}>
         {data &&
           data.map((product) => (
-            <Grid item key={product._id}>
-              <BasicProductCard product={product} />
-            </Grid>
+            <>
+              <Grid item key={product._id}>
+                <BasicProductCard product={product} />
+              </Grid>
+            </>
           ))}
       </Grid>
     </Box>
