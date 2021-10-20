59f62962a1834916ede00c197c1aecb94e34adc6
Axel Thiel <axth2220@gmail.com>
2021-10-20 09:42:11 +0200

    add category form

--- client/src/components/Form/CategoryForm.js
+++ client/src/components/Form/CategoryForm.js
@@ -1,12 +1,53 @@
 import React, {useEffect, useState} from 'react'
+import useInput from '../../hooks/useInput';
 import { category } from "../../constants/formFields";
+import { addCategory } from '../../api/api';
+import { FormGenerator } from './FormGenerator';
+import { Button } from '@mui/material';
 
 
-export const CategoryForm = () => {
+export const CategoryForm = ({exitForm}) => {
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  const titleInput = useInput(category.title.validate)
+  const thumbnailInput = useInput(category.thumbnail.validate)
+  const descriptionInput = useInput(category.description.validate)
+
+  const inputs = [
+    {...titleInput, ...category.title},
+    {...thumbnailInput, ...category.thumbnail},
+    {...descriptionInput, ...category.description},
+  ];
+
+  const handleSubmit = async () => {
+    if (!formIsValid) {
+      return;
+    } else {
+      const data = {
+        title: titleInput.value,
+        thumbnail: thumbnailInput.value,
+        description: descriptionInput.value
+      }
+      try {
+        const response = await addCategory(data)
+        console.log('category added')
+        exitForm()  
+      } catch (error) {
+        console.log('Register failed.')
+      }
+    }
+  };
+
+  useEffect(() => {
+    setFormIsValid(
+    titleInput.isValid &&
+    thumbnailInput.isValid &&
+    descriptionInput.isValid)
+  }, [titleInput.isValid, thumbnailInput.isValid, descriptionInput.isValid])
+
   return (
-    <div>
-      
-    </div>
+    <FormGenerator inputs={inputs}>
+      <Button onClick={handleSubmit} disabled={!formIsValid}>Registrera</Button>
+    </FormGenerator>
   )
 }
-
