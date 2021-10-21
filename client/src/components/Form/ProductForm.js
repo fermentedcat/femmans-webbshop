a5eeeee5c758bb65146322a92c7fa5c74a5675ad
Axel Thiel <axth2220@gmail.com>
2021-10-21 11:34:07 +0200

    add functionality to edit products in product form

--- client/src/components/Form/ProductForm.js
+++ client/src/components/Form/ProductForm.js
@@ -7,16 +7,16 @@ import { product } from '../../constants/formFields'
 import { FormGenerator } from './FormGenerator'
 import { useFetch } from '../../hooks/useFetch'
 
-export const ProductForm = ({ exitForm, updateList }) => {
+export const ProductForm = ({exitForm, addToList, handleEdit, productToEdit = null }) => {
   const [formIsValid, setFormIsValid] = useState(false)
-
-  const titleInput = useInput(product.title.validate)
-  const descriptionInput = useInput(product.description.validate)
-  const priceInput = useInput(product.price.validate)
-  const brandInput = useInput(product.brand.validate)
   const [categoryValue, setCategoryValue] = useState("")
-  const weightInput = useInput(product.weight.validate)
-  const photoInput = useInput(product.photo.validate)
+
+  const titleInput = useInput(product.title.validate, productToEdit ? productToEdit.title : "")
+  const descriptionInput = useInput(product.description.validate, productToEdit ? productToEdit.description : "")
+  const priceInput = useInput(product.price.validate, productToEdit ? productToEdit.price : "")
+  const brandInput = useInput(product.brand.validate, productToEdit ? productToEdit.brand : "")
+  const weightInput = useInput(product.weight.validate, productToEdit ? productToEdit.weight : "")
+  const photoInput = useInput( product.photo.validate, productToEdit ? productToEdit.photo: "")
 
   const { data: categories } = useFetch(getCategories)
 
@@ -56,10 +56,11 @@ export const ProductForm = ({ exitForm, updateList }) => {
       try {
         const response = await addProduct(data)
         if (response.data) {
-          updateList(response.data)
-          exitForm()
+          addToList(response.data)
+          // exitForm()
         }
       } catch (error) {
+        console.log(error)
         console.log('Failed saving product.')
       }
     }
@@ -84,12 +85,23 @@ export const ProductForm = ({ exitForm, updateList }) => {
   ])
 
   const button = (
-    <Button onClick={handleSubmit} disabled={!formIsValid}>
+    <Button onClick={productToEdit ? () => handleEdit({
+      _id: productToEdit._id,
+      title: titleInput.value,
+      description: descriptionInput.value,
+      price: priceInput.value,
+      brand: brandInput.value,
+      categories: [categoryValue],
+      weight: weightInput.value,
+      photos: [photoInput.value],
+    }, productToEdit._id ) : handleSubmit} disabled={!formIsValid}>
       Spara
     </Button>
   )
 
+  console.log(inputs)
+
   return (
-    <FormGenerator inputs={inputs} onSumbit={handleSubmit} button={button} />
+    <FormGenerator inputs={inputs} button={button} />
   )
 }
