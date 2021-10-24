b0109be3ba7a8c87e9f8bb228fdd3ab39caf83ff
Maja Thunberg <majaneko@gmail.com>
2021-10-24 09:48:01 +0200

    Render modal in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import './App.css';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { AuthContext } from './context/authContext';
 import { UiContext } from './context/uiContext';
 
@@ -10,23 +10,40 @@ import { AdminPage } from './pages/AdminPage';
 import { LandingPage } from './pages/LandingPage';
 import { ProductsPage } from './pages/ProductsPage';
 import { Header } from './components/Layout/Header';
+import { BasicModal } from './components/Layout/BasicModal';
+import { ModalContent } from './components/Layout/ModalContent';
 
 function App() {
   const { isAuthenticated, email } = useContext(AuthContext);
-  const { notification } = useContext(UiContext);
+  const { modal, closeModal, notification } = useContext(UiContext);
 
   return (
-    <Container disableGutters sx={{ width: '75vw', height: '100vh', backgroundColor: '#F6F4F4', mx: 'auto' }}>
+    <Container
+      disableGutters
+      sx={{
+        width: '75vw',
+        height: '100vh',
+        backgroundColor: '#F6F4F4',
+        mx: 'auto',
+      }}
+    >
       <Header />
       <Box sx={{ padding: 2 }}>
-        { notification.show && <p>{notification.type}: {notification.message}</p>}
-        { isAuthenticated ? <p>Welcome, {email}</p> : <p>Not logged in</p>}
+        {notification.show && (
+          <p>
+            {notification.type}: {notification.message}
+          </p>
+        )}
+        {isAuthenticated ? <p>Welcome, {email}</p> : <p>Not logged in</p>}
         <Switch>
           <Route path="/all-products" component={ProductsPage} />
           <Route path="/admin" component={AdminPage} />
           <Route path="/" component={LandingPage} />
         </Switch>
       </Box>
+      <BasicModal open={modal.show} onClose={closeModal}>
+        <ModalContent />
+      </BasicModal>
     </Container>
   );
 }
