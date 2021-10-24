75403ce9e8291c15caf3d6818de5d4b89fabd0d2
Maja Thunberg <majaneko@gmail.com>
2021-10-24 10:51:54 +0200

    Move table styled components into own files

---
+++ client/src/components/Table/styled/SectionHeaderRow.js
@@ -0,0 +1,10 @@
+import { styled } from '@mui/material/styles';
+import { TableRow } from '@mui/material'
+
+export const SectionHeaderRow = styled(TableRow)(() => ({
+  background: '#292828',
+  th: {
+    color: '#F9F9F9',
+    fontWeight: 700,
+  },
+}));
