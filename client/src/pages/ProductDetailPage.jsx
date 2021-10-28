c7ca9c0352a1f22b9ba202105d7179f2b1fd00be
GustafJ <gustaf89@hotmail.se>
2021-10-28 11:59:28 +0200

    work with ui add brand categories

--- client/src/pages/ProductDetailPage.jsx
+++ client/src/pages/ProductDetailPage.jsx
@@ -1,34 +1,67 @@
 import React from 'react';
 import { useFetch } from '../hooks/useFetch';
 import { getProduct } from '../api/api';
-import { Typography, Box, Button } from '@mui/material';
+import { Typography, Box, Button, List, ListItem } from '@mui/material';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { Link } from 'react-router-dom';
 
 export const ProductDetailPage = (props) => {
   const prodId = props.match.params.id;
   const { data: product } = useFetch(getProduct, prodId);
-  console.log(product);
   return (
     <Box>
       {product && (
         <>
           <Typography variant='h3'>{product.title}</Typography>
-          <Box sx={{ display: 'flex' }}>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
             <Box>
-              <img src={product.photos[0]} alt='product.title' />
+              <img
+                src={product.photos[0]}
+                alt='product.title'
+                style={{ maxWidth: '500px', minWidth: '200px' }}
+              />
             </Box>
-            <Box sx={{ width: '100%' }}>
+            <Box
+              sx={{
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-around',
+              }}
+            >
               <Button
-                sx={{ width: '90%', margin: '0 5%' }}
+                sx={{ width: '50%', margin: '0 5%', alignSelf: 'center' }}
                 id='button'
                 variant='contained'
               >
                 <AddShoppingCartIcon sx={{ mr: '3px' }} />
                 <Typography>{product.price} kr</Typography>
               </Button>
+              <Box sx={{ margin: '0 5%' }}>
+                <Typography variant='h6'>Kategorier</Typography>
+                <List>
+                  {product.categories.map((category) => (
+                    <Link
+                      key={category._id}
+                      to={`/category/${category.title.toLowerCase()}`}
+                    >
+                      <ListItem>
+                        <Typography key={category._id}>
+                          {category.title}
+                        </Typography>
+                      </ListItem>
+                    </Link>
+                  ))}
+                </List>
+                <Typography variant='h6'>Tillverkare</Typography>
+                <Typography variant='body2'>{product.brand}</Typography>
+              </Box>
             </Box>
           </Box>
-          <Typography variant='body1'>{product.description}</Typography>
+          <Box>
+            <Typography variant='h4'>Beskrivning</Typography>
+            <Typography variant='body2'>{product.description}</Typography>
+          </Box>
         </>
       )}
     </Box>
