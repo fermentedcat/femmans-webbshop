3af4a8d6c96917471bd1ba6cad7e7e41e568ad97
GustafJ <gustaf89@hotmail.se>
2021-10-28 10:38:28 +0200

    add button, desc and image

--- client/src/pages/ProductDetailPage.jsx
+++ client/src/pages/ProductDetailPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useFetch } from '../hooks/useFetch';
 import { getProduct } from '../api/api';
-import Box from '@mui/material/Box';
-import { Typography } from '@mui/material';
+import { Typography, Box, Button } from '@mui/material';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 export const ProductDetailPage = (props) => {
   const prodId = props.match.params.id;
@@ -13,7 +13,22 @@ export const ProductDetailPage = (props) => {
       {product && (
         <>
           <Typography variant='h3'>{product.title}</Typography>
-          <img src={product.photos[0]} alt='product.title' />
+          <Box sx={{ display: 'flex' }}>
+            <Box>
+              <img src={product.photos[0]} alt='product.title' />
+            </Box>
+            <Box sx={{ width: '100%' }}>
+              <Button
+                sx={{ width: '90%', margin: '0 5%' }}
+                id='button'
+                variant='contained'
+              >
+                <AddShoppingCartIcon sx={{ mr: '3px' }} />
+                <Typography>{product.price} kr</Typography>
+              </Button>
+            </Box>
+          </Box>
+          <Typography variant='body1'>{product.description}</Typography>
         </>
       )}
     </Box>
