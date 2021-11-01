bcb4c03790b94846d9350c164b1eff60017c8243
Maja Thunberg <majaneko@gmail.com>
2021-11-01 12:50:09 +0100

    Add console.log

--- client/src/components/Layout/BasicProductGrid.js
+++ client/src/components/Layout/BasicProductGrid.js
@@ -1,13 +1,17 @@
+/* eslint-disable no-console */
 import React from 'react';
 import Grid from '@mui/material/Grid';
 import BasicProdCard from './BasicProductCard';
 
-export const BasicProductGrid = ({ products }) => (
-  <Grid container spacing={4} sx={{ justifyContent: 'center' }}>
-    {products.map((product) => (
-      <Grid item key={product._id}>
-        <BasicProdCard product={product} />
-      </Grid>
-    ))}
-  </Grid>
-);
+export const BasicProductGrid = ({ products }) => {
+  console.log(products);
+  return (
+    <Grid container spacing={4} sx={{ justifyContent: 'center' }}>
+      {products.map((product) => (
+        <Grid item key={product._id}>
+          <BasicProdCard product={product} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
