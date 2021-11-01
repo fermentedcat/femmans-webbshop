6ca040e0a9f0b36f20de2fea5111f2e3c03c126d
Maja Thunberg <majaneko@gmail.com>
2021-11-01 12:22:52 +0100

    change api base url to server url

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -2,6 +2,6 @@ export const DATA_CATEGORIES = [
   { category: 'Products', endpoint: 'products' },
   { category: 'Orders', endpoint: 'orders' },
   { category: 'Categories', endpoint: 'categories' },
-];
+]
 
-export const API_BASE_URL = 'http://localhost:3000/api/';
+export const API_BASE_URL = 'femmans-api.herokuapp.com/api/'
